9c7dfde17e3fcb12ff13e747df205b35339ebcbf
Frederick Lee <frederick.v.l@gmail.com>
2026-02-13 15:48:54 -0500

    Switch to stable NVIDIA and update lockfile

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -7,7 +7,7 @@
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;  # use the open kernel module
-    package = config.boot.kernelPackages.nvidiaPackages.latest; # switch to stable if something breaks
+    package = config.boot.kernelPackages.nvidiaPackages.stable; # switch to stable if something breaks
 
     powerManagement.enable = false;
     nvidiaPersistenced = true;
