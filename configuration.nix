cbafd9756f69539300dd1e0067d9a4534c5c5556
Frederick Lee <frederick.v.l@gmail.com>
2026-02-13 16:31:31 -0500

    Pin kernel to 6.18

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_18;
 
   # ===== Networking =====
   networking.hostName = lib.mkDefault "generic-nix";
