4c1c7adc3437598b62cb11f6b3e81fa887cbe592
Frederick Lee <frederick.v.l@gmail.com>
2026-03-06 05:46:19 -0500

    play with gnome-ubuntu config

--- desktop/gnome-ubuntu.nix
+++ desktop/gnome-ubuntu.nix
@@ -1,22 +1,41 @@
 # Replicate the Ubuntu feel on NixOS.
-{ config, pkgs, ... }@inputs:
+{ lib, config, pkgs, ... }@inputs:
 let 
   gnome = import ./gnome.nix inputs;
   gnomeExtensions = with pkgs.gnomeExtensions; [ dash-to-dock appindicator gtk4-desktop-icons-ng-ding ];
 in
 gnome // {
   environment.systemPackages = with pkgs; [ yaru-theme ] ++ gnomeExtensions;
-  services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
-    [org.gnome.desktop.interface]
-    gtk-theme='Yaru'
-    icon-theme='Yaru'
-    cursor-theme='Yaru'
+  # services.desktopManager.gnome.extraGSettingsOverrides = ''
+  #   [org.gnome.desktop.interface]
+  #   gtk-theme='Yaru'
+  #   icon-theme='Yaru'
+  #   cursor-theme='Yaru'
 
-    [org.gnome.shell.extensions.dash-to-dock]
-    dock-position='LEFT'
-    extend-height=true
-    dash-max-icon-size=48
-    autohide=true
-  '';
+  #   [org.gnome.shell.extensions.dash-to-dock]
+  #   dock-position='LEFT'
+  #   extend-height=true
+  #   dash-max-icon-size=48
+  #   autohide=true
+  # '';
+  programs.dconf = {
+    enable = true;
+    profiles.user.databases = [{
+      settings = {
+        "org/gnome/shell".enabled-extensions = map (ex: ex.extensionUuid) gnomeExtensions;
+        "org/gnome/shell/extensions/dash-to-dock" = {
+          dock-position = "LEFT";
+          extend-height = true;
+          dash-max-icon-size = lib.gvariant.mkInt32 48;
+          autohide = true;
+        };
+        "org/gnome/desktop/interface" = {
+          gtk-theme = "Yaru";
+          icon-theme = "Yaru";
+          cursor-theme = "Yaru";
+        };
+      };
+    }];
+  };
   fonts.packages = with pkgs; [ ubuntu-sans ];
 }
