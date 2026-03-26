33aaae307f5bfad6cbcd15b5450a826fa41fdcc5
Frederick Lee <frederick.v.l@gmail.com>
2026-03-26 13:38:12 -0400

    keep NVIDIA vaapi up to date for video acceleration

--- hardware/nvidia.nix
+++ hardware/nvidia.nix
@@ -1,6 +1,12 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 {
   hardware.graphics.enable = true;
+
+  # Use latest vaapi driver
+  hardware.nvidia.videoAcceleration = false;
+  hardware.graphics.extraPackages = with pkgs-unstable; [
+    nvidia-vaapi-driver
+  ];
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
