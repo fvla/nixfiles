7bc28915e360f0c1bd772e9652eccaf8061e0308
Frederick Lee <frederick.v.l@gmail.com>
2026-04-28 13:55:45 -0400

    failed MIME attempt with dolphin

--- desktop/hyprland.nix
+++ desktop/hyprland.nix
@@ -36,6 +36,27 @@
   services.dbus.enable = true;
   services.dbus.packages = [ pkgs.gcr ];
 
+  xdg.mime.enable = true;
+
+  # Add overlay
+  nixpkgs.overlays = [
+    (final: prev: {
+      kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
+        dolphin = prev.symlinkJoin {
+          name = "dolphin-wrapped";
+          paths = [ kprev.dolphin ];
+          nativeBuildInputs = [ prev.makeWrapper ];
+          postBuild = ''
+            rm $out/bin/dolphin
+            makeWrapper ${kprev.dolphin}/bin/dolphin $out/bin/dolphin \
+              --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
+              --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+          '';
+        };
+      });
+    })
+  ];
+
   fonts.packages = with pkgs-unstable; [
     font-awesome # for default waybar
     vista-fonts # consolas my beloved
