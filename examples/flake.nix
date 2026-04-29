d082a65f940d92c6db2fda45786a9fc924f72099
Frederick Lee <frederick.v.l@gmail.com>
2026-04-29 15:22:39 -0400

    update examples

--- examples/flake.nix
+++ examples/flake.nix
@@ -1,18 +1,19 @@
 {
   inputs = {
-    # This flake depends on nixfiles...
     nixfiles.url = "github:fvla/nixfiles";
-    # ...and nixfiles depends on this flake. This works because the outputs themselves have no circular dependencies.
-    nixfiles.inputs.sysflake.follows = "";
   };
 
   outputs = { self, nixfiles, ... }:
-  with nixfiles; {
-    # Pass through the NixOS configurations from nixfiles.
-    inherit nixosConfigurations;
-
-    # Pseudo-circular dependency: nixfiles.nixosConfigurations imports this flake to get the nixosModules.
-    nixosModules.hardware-configuration = import ./hardware-configuration.nix;
-    nixosModules.users = import ./users.nix;
+  let
+    extraModules = [
+      ./users.nix
+    ];
+  in
+  {
+    # Extend each evaluated NixOS system with local modules
+    nixosConfigurations =
+      nixfiles.inputs.nixpkgs.lib.mapAttrs
+        (_: system: system.extendModules { modules = extraModules; })
+        nixfiles.nixosConfigurations;
   };
 }
